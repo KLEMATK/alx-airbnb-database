85ef416971a9a459dfc1eaa435f5ea5983709c26
KLEM <thompsonking1991@gmail.com>
2025-08-25 23:22:15 -0700

    Fix JOIN queries: add ORDER BY to LEFT JOIN and ensure proper FULL OUTER JOIN

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,15 +1,14 @@
 -- 1) INNER JOIN: bookings with users
-SELECT 
+SELECT
     b.id AS booking_id,
     b.booking_date,
     u.id AS user_id,
-    u.name AS user_name,
-    u.email
+    u.name AS user_name,    u.email
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
 -- 2) LEFT JOIN: properties with reviews (including properties with no reviews)
-SELECT 
+SELECT
     p.id AS property_id,
     p.name AS property_name,
     r.id AS review_id,
@@ -19,18 +18,4 @@ FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
 
 -- 3) FULL OUTER JOIN (MySQL equivalent using UNION)
-SELECT 
-    u.id AS user_id,
-    u.name AS user_name,
-    b.id AS booking_id,
-    b.booking_date
-FROM users u
-LEFT JOIN bookings b ON u.id = b.user_id
-UNION
-SELECT 
-    u.id AS user_id,
-    u.name AS user_name,
-    b.id AS booking_id,
-    b.booking_date
-FROM users u
-RIGHT JOIN bookings b ON u.id = b.user_id;
+
